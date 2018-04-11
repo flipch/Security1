7dd43efaa2af47311acfa40d7ec74433639d3179
Felipe Heliszkowski <felipech@outlook.com>
2018-04-11 15:44:01 +0100

    Client login handling

---
+++ Security47064/src/com/sc/client/Client.java
@@ -0,0 +1,110 @@
+package com.sc.client;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import com.sc.utilities.Pair;
+
+/**
+ * @author Felipe
+ *
+ */
+public class Client {
+
+	private static final String IPPort = "(\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}):(\\d{1,5})";
+	private static final Pattern PATTERN = Pattern.compile(IPPort);
+
+	/**
+	 * Run client
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Check if folder for server files exist else create.
+		File serverFolder = new File("Client");
+		if (!serverFolder.exists()) {
+			if (!serverFolder.mkdir())
+				// Folder creation failed
+				System.err.println("[" + LocalDateTime.now() + "] " + "Failed creating server folder!");
+			return;
+		}
+
+		// Folder created we can start our server.
+		try {
+			System.out.println("[" + LocalDateTime.now() + "] " + "Welcome to PhotoShare.");
+			startClient(args);
+		} catch (SocketException se) {
+			System.err.println(se.getMessage());
+		}
+
+	}
+
+	private static void startClient(String[] args) throws SocketException {
+		try {
+			Socket socket;
+			Scanner input = new Scanner(System.in);
+
+			// Make connection
+			String[] untreatedSv = args[2].split(":");
+			String ip = untreatedSv[0];
+			int sock = Integer.parseInt(untreatedSv[1]);
+			Pair<String, Integer> connectionSettings = new Pair<String, Integer>(ip, sock);
+			socket = new Socket(connectionSettings.first(), connectionSettings.second());
+
+			// Log in or exit if wrong password.
+			handleLogIn(input, socket, args[0], args[1]);
+
+			socket.close();
+			input.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void handleLogIn(Scanner input, Socket socket, String username, String pwd) {
+		try {
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+
+			// Send our user and pw.
+			out.writeObject(username);
+			out.writeObject(pwd);
+
+			// Check our answer.
+			Pair<Boolean, String> res = (Pair<Boolean, String>) in.readObject();
+			if (res.first()) {
+				System.out.println("[" + LocalDateTime.now() + "] " + "Bem vindo " + username);
+				String userDir = "Clients/" + username;
+				File dir = new File(userDir);
+				if (!dir.exists())
+					dir.mkdir();
+
+				// All good and logged in lets handle commands
+				handleCommands(input, socket, username, pwd);
+			} else {
+				System.out.println(res.second());
+			}
+
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void handleCommands(Scanner input, Socket socket, String username, String pwd) {
+		System.out.println("[" + LocalDateTime.now() + "] " + "Escolha uma operacao:");
+		System.out.println("[" + LocalDateTime.now() + "] "
+				+ "[ -a <photos> | -l <userId> | -i <userId> <photo> | -g <userId> \n"
+				+ "| -c <comment> <userId> <photo> | -L <userId> <photo> | \n -D <userId> <photo> | -f <followUserIds> | -r <followUserIds> ]");
+
+	}
+
+}
